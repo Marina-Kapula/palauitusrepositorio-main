fda057aa782b29be2ace8e24be44469d722251c1
Maryna Kapula <maryna.kapula@gmail.com>
2025-10-03 13:24:57 +0300

    comit jopa

---
+++ osa1/anekdootit/1.12/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react"; 
+import "./App.css";
+import Buttons from "./Buttons.jsx";
+import Statistics from "./Statistics.jsx";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const buttonNames = ["Good", "Neutral", "Bad"];
+
+  return (
+    <div className="app">
+      <h1>Give Feedback</h1>
+      <Buttons 
+        buttonNames={buttonNames} 
+        setGood={setGood} 
+        setNeutral={setNeutral} 
+        setBad={setBad} 
+      />
+
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
