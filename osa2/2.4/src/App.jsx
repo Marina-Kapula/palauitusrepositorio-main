e2d62b368105da1f2d1da1c6625583feaaae476b
Maryna Kapula <maryna.kapula@gmail.com>
2025-10-08 13:38:58 +0300

    2.4 valmis

--- osa2/2.4/src/App.jsx
+++ osa2/2.4/src/App.jsx
@@ -1,52 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "./Index.css";
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 },
-      { name: 'Redux', exercises: 11 }
+  const courses = [
+      {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        { name: 'Fundamentals of React', exercises: 10, id: 1 },
+        { name: 'Using props to pass data', exercises: 7, id: 2 },
+        { name: 'State of a component', exercises: 14, id: 3 },
+        { name: 'Redux', exercises: 11, id: 4 }
+      ]
+      },
+      {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        { name: 'Routing', exercises: 3, id: 1 },
+        { name: 'Middlewares', exercises: 7, id: 2 }
+      ]
+      }
+      ]
 
-    ]
+  const Header = ({ name }) => <h1>{name}</h1>
+
+  const Content = ({ parts }) => (
+    <div>
+      {parts.map(part => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </div>
+  )
+
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return <p>Number of exercises {total}</p>
   }
- 
-  function Header({ course }) {
-    return <h1>{course}</h1>
-  }
 
-
-  function Content({ parts }) {
-    return (
-      <div>
-        {parts.map((part, index) => (
-          <p key={index}>
-            {part.name} {part.exercises}
-          </p>
-        ))}
-      </div>
-    )
-  }
-
-  function Total({ parts }) {
-    const total = parts.reduce((sum, part) => {
-      console.log('what is happening', sum, part)
-      return sum + part.exercises
-    }, 0)
-
-
-  return <p>Number of exercises {total}</p>
+  return (
+    <div className="container">
+      {courses.map(course => (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
+    </div>
+  )
 }
-
-return ( <div className='box'>
-   <Header course={course.name} 
-/> <Content parts={course.parts} 
-/> <Total parts={course.parts} /> 
-</div> ) }
-
 
 export default App
