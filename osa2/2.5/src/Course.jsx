fa11caf3a9f50bb98e408b546f11782102fcd789
Maryna Kapula <maryna.kapula@gmail.com>
2025-10-10 11:36:06 +0300

    2.5 valmis

---
+++ osa2/2.5/src/Course.jsx
@@ -0,0 +1,32 @@
+// Course.jsx
+
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><strong>Number of exercises {total}</strong></p>
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+export default Course
