0d120bbe7a7e36f026b119c0ee4829e1ab4a2fbb
Marina Kapula <maryna.kapula@gmail.com>
2025-10-13 13:54:17 +0300

    e567

--- osa2/2.8/src/App.jsx
+++ osa2/2.8/src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './Index.css'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+   const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    // === Перевірка на дублікати ===
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName}  is already added to phonebook`)
+      return
+    }
+    // === Кінець перевірки ===
+
+    const personObject = { name: newName }
+    const updatedPersons = persons.concat(personObject)
+
+    setPersons(updatedPersons)
+    setNewName('')
+    number: newNumber
+
+    console.log('names:', updatedPersons.map(p => p.name))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+          <button type="submit">Add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <p key={person.name}>{person.name}</p>
+      )}
+
+      <div>: {newName}</div> {/* для перевірки */}
+    </div>
   )
 }
 
