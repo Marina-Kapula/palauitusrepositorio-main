f03dbe3e6c280ceacd7be82761e1a2a1fd18bb70
Marina Kapula <maryna.kapula@example.com>
2025-10-13 13:04:44 +0300

    2.7 valmis

--- osa2/2.7/src/App.jsx
+++ osa2/2.7/src/App.jsx
@@ -4,13 +4,18 @@ import './App.css'
 
 
 const App = () => {
- 
-const [persons, setPersons] = useState([]) 
-const [newName, setNewName] = useState('')
-
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
+
+    // === Перевірка на дублікати ===
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName}  is already added to phonebook`)
+      return
+    }
+    // === Кінець перевірки ===
 
     const personObject = { name: newName }
     const updatedPersons = persons.concat(personObject)
@@ -28,28 +33,21 @@ const [newName, setNewName] = useState('')
   return (
     <div>
       <h2>Phonebook</h2>
-<form onSubmit={addPerson}>
-  <div>
-    name: <input value={newName} onChange={handleNameChange} />
-    <button type="submit">Add</button>  {/* кнопка сразу после input */}
-  </div>
-</form>
-
-
-
-{/* === список сраних iмен === */}
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+          <button type="submit">Add</button>
+        </div>
+      </form>
 
       <h2>Numbers</h2>
       {persons.map(person => 
         <p key={person.name}>{person.name}</p>
       )}
 
-      <div>: {newName}</div> {/* строка для провірки */}
+      <div>: {newName}</div> {/* для перевірки */}
     </div>
   )
 }
-
-
-
 
 export default App
