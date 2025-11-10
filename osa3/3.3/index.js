79f3d4c71d7b43a22f508255da49c4a65a1632de
Maryna Kapula <maryna.kapula@gmail.com>
2025-11-10 12:55:24 +0200

    3.3 valmis

---
+++ osa3/3.3/index.js
@@ -0,0 +1,30 @@
+console.log('Server file loaded')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+let persons = [
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345" }
+]
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
