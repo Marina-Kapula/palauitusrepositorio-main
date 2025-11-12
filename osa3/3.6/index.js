c7c944ccc2b8880270fcc0f0588406ef24bcde0b
Maryna Kapula <maryna.kapula@gmail.com>
2025-11-12 12:58:00 +0200

    3.6 valmis

---
+++ osa3/3.6/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(express.json())
+
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345" }
+]
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'name or number missing' })
+  }
+
+ 
+  const nameExists = persons.some(p => p.name === body.name)
+  if (nameExists) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+
+  
+  const newPerson = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  }
+
+  persons.push(newPerson)
+  res.status(201).json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
